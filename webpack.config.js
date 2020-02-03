20b2f5eb7facda8d59ac238cbabed81413603293
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-02-03 14:14:20 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname),
+        filename: "./dist/bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", "*"]
+    }
+};
